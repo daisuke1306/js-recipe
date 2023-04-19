0363f9e586e2ec4ef99eb01cabb095a2061d2f0b
Daisuke <daisuke.1128.zakky@gmail.com>
2023-04-19 15:56:00 +0900

    [カウンター]を学習しました。

---
+++ 01_テキスト/01_JavaScriptのイベント/main.js
@@ -0,0 +1,22 @@
+const button = document.getElementById("button")
+
+const alertMessage = function () {
+  alert("クリックしたね")
+}
+
+// 関数を登録
+button.onclick = alertMessage
+
+const inputText = document.getElementById("input-text")
+const inputDate = document.getElementById("input-date")
+
+const logValue = function (e) {
+  console.log(e.target.value)
+}
+
+document.onkeydown = function (e) {
+  console.log(e.key)
+}
+
+inputText.oninput = logValue
+inputDate.oninput = logValue
